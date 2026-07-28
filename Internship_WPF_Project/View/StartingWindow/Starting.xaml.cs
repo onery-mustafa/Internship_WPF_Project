5bc3e30fb0a9e35e9ed27d3c789952a0b70d51d3
onery-mustafa <onery.mustafa@gmail.com>
2026-07-28 13:40:03 +0300

    trial

---
+++ Internship_WPF_Project/View/StartingWindow/Starting.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Internship_WPF_Project.View.StartingWindow
+{
+    public partial class Starting : Window
+    {
+        public Starting()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
