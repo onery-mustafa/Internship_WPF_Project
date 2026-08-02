741563d3f01dfff36b8e925175dd497d1334aa71
Mustafa ÖNERYILDIZ <onery.mustafa@gmail.com>
2026-08-02 15:32:10 +0300

    trial

---
+++ Internship_WPF_Project/View/IP/ip.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Internship_WPF_Project.View.IP
+{
+    /// <summary>
+    /// Interaction logic for ip.xaml
+    /// </summary>
+    public partial class ip : Window
+    {
+        public ip()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Internship_WPF_Project/View/SignIn/textbox.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Internship_WPF_Project.View.SignIn
+{
+    /// <summary>
+    /// Interaction logic for textbox.xaml
+    /// </summary>
+    public partial class textbox : UserControl
+    {
+        public textbox()
+        {
+            InitializeComponent();
+        }
+
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set
+            {
+                placeholder = value;
+                tbPlaceHolder.Text = placeholder; // Yer tutucu metnini güncelle
+
+            }
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text))
+            {
+                tbPlaceHolder.Visibility = Visibility.Visible; // TextBox boşsa yer tutucu metnini göster
+            }
+            else
+            {
+                tbPlaceHolder.Visibility = Visibility.Hidden; // TextBox doluysa yer tutucu metnini gizle
+            }
+        }
+
+        private void btnClearClick(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus(); // kutu temizlendikten sonra imleci tekrar kutuya odaklamak için
+        }
+    }
+}
